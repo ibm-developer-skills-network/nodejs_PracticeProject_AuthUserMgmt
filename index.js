8f4d24803f301c7f374f70c409abdfe15857dd8f
PhuongAnhLy <phuonganh.pal27@gmail.com>
2023-11-07 10:29:46 -0500

    update

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const routes = require('./router/friends.js')
 
 let users = []
 
+/* 2nd:
+provide a manner in which it can be checked 
+to see if the username exists in the list of registered users
+to avoid duplications and keep the username unique
+this is a utility function and not an endpoint
+*/
 const doesExist = (username)=>{
   let userswithsamename = users.filter((user)=>{
     return user.username === username
@@ -16,6 +22,11 @@ const doesExist = (username)=>{
   }
 }
 
+/* 3rd:
+check if the username and password match what you have in the list of registered users
+It returns a boolean depending on whether the credentials match or not
+This is also a utility function and not an endpoint
+*/
 const authenticatedUser = (username,password)=>{
   let validusers = users.filter((user)=>{
     return (user.username === username && user.password === password)
@@ -29,10 +40,21 @@ const authenticatedUser = (username,password)=>{
 
 const app = express();
 
+/* 4th:
+create and use a session object with user-defined secret
+as a middleware to intercept the requests and 
+ensure that the session is valid before processing the request
+*/
 app.use(session({secret:"fingerpint"},resave=true,saveUninitialized=true));
 
 app.use(express.json());
 
+/* 6th:
+ensures that all the endpoints starting with /friends go through the middleware
+It retrieves the authorization details from the session and verifies it.
+If the token is validated, the user is aunteticated and the control is passed on to the next endpoint handler
+If the token is invalid, the user is not authenticated and an error message is returned.
+*/
 app.use("/friends", function auth(req,res,next){
    if(req.session.authorization) {
        token = req.session.authorization['accessToken'];
@@ -50,14 +72,19 @@ app.use("/friends", function auth(req,res,next){
     }
 });
 
+/* 5th:
+provide an endpoint for the registered users to login
+*/
 app.post("/login", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
-
+  //Return an error if the username or password is not provided
   if (!username || !password) {
       return res.status(404).json({message: "Error logging in"});
   }
-
+  //Creates an access token that is valid for 1 hour (60 X 60 seconds) and logs the user in
+  //if the credentials are correct
+  //Throws an error, if the credentials are incorrect
   if (authenticatedUser(username,password)) {
     let accessToken = jwt.sign({
       data: password
@@ -72,6 +99,12 @@ app.post("/login", (req,res) => {
   }
 });
 
+/* 1st:
+to provide access to the API endpoints only to the authenticated users
+you need to provide a way to register the users
+this endpoint will be a post request that accepts username and password through the body
+the user doesn’t have to be authenticated to access this endpoint.
+*/
 app.post("/register", (req,res) => {
   const username = req.body.username;
   const password = req.body.password;
--- router/friends.js
+++ router/friends.js
@@ -14,34 +14,67 @@ router.get("/",(req,res)=>{
 
   // Update the code here
 
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  res.send(JSON.stringify(friends,null,4))//This line is to be replaced with actual return value
 });
 
 // GET by specific ID request: Retrieve a single friend with email ID
 router.get("/:email",(req,res)=>{
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  res.send(friends[email])//This line is to be replaced with actual return value
 });
 
 
 // POST request: Add a new friend
 router.post("/",(req,res)=>{
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  if (req.body.email){
+      friends[req.body.email] = {
+          "firstName":req.body.firstName,
+          "lastName":req.body.lastName,
+          "DOB":req.body.DOB
+      }
+  }
+  res.send("The user" + (' ') + (req.body.firstName) + "has been added!");//This line is to be replaced with actual return value
 });
 
 
 // PUT request: Update the details of a friend with email id
 router.put("/:email", (req, res) => {
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  let friend = friends[email]
+  if (friend) {
+      let DOB = req.body.DOB;
+      let firstName = req.body.firstName;
+      let lastName = req.body.lastName;
+
+      if (DOB) {
+          friend["DOB"] = DOB
+      }
+      if (firstName) {
+          friend["firstName"] = firstName;
+      }
+      if (lastName) {
+          friend["lastName"] = lastName;
+      }
+          friend["email"] = email;
+          res.send(`Friend with the email  ${email} updated.`);
+  }
+  else {
+  res.send("Unable to find friend!");
+  }//This line is to be replaced with actual return value
 });
 
 
 // DELETE request: Delete a friend by email id
 router.delete("/:email", (req, res) => {
   // Update the code here
-  res.send("Yet to be implemented")//This line is to be replaced with actual return value
+  const email = req.params.email;
+  if (email) {
+      delete friends[email]
+  }
+  res.send(`Friend with the email ${email} deleted.`);//This line is to be replaced with actual return value
 });
 
 module.exports=router;
